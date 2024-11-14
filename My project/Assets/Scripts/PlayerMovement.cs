7d3e3bc9e64ee96a5a25e17dc2c393bc95baac62
Madhav Gangneja <kindamad.hav@gmail.com>
2024-11-14 20:13:14 +0530

    .

--- My project/Assets/Scripts/PlayerMovement.cs
+++ My project/Assets/Scripts/PlayerMovement.cs
@@ -7,17 +7,25 @@ public class PlayerMovement : MonoBehaviour
 {
     public float playerSpeed = 5f;
     public float jumpForce = 5f;
+    public float slideSpeed = 8f;
+    public float slideDuration = 1f;
+    private float normalSpeed;
 
 
     private Rigidbody2D rb;
+
     private Animator animator;
 
     private bool isGrounded = true;
+    private bool isSliding = false;
+    
+
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
+        normalSpeed = playerSpeed;
     }
 
     // Update is called once per frame
@@ -28,9 +36,19 @@ public class PlayerMovement : MonoBehaviour
         {
             rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
             isGrounded = false;
-            animator.SetBool("isJumping", true);
+            animator.Play("Jump");
+            //animator.SetBool("isJumping", true);
         }
-      
+
+        if (Input.GetKeyDown(KeyCode.S) && isGrounded && !isSliding)
+        {
+            isSliding = true;
+            playerSpeed = slideSpeed;
+            animator.SetBool("isSliding", true);
+            StartCoroutine(StopSlidingAfterDuration());
+        }
+
+
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
@@ -38,7 +56,15 @@ public class PlayerMovement : MonoBehaviour
         if (collision.gameObject.CompareTag("Ground"))
         {
             isGrounded = true;
-            animator.SetBool("isJumping", false);
+            //animator.SetBool("isJumping", false);
         }
+    }
+
+    private IEnumerator StopSlidingAfterDuration()
+    {
+        yield return new WaitForSeconds(slideDuration);
+        isSliding = false;
+        playerSpeed = normalSpeed;
+        animator.SetBool("isSliding", false);
     }
 }
